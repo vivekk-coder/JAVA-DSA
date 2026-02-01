d49b7da27774962d174c616867f668c1646e8d7e
vivekk-coder <vivakkori64@gmail.com>
2026-02-01 23:24:51 +0530

    Add space in pyramid num

--- src/PyramidNum.java
+++ src/PyramidNum.java
@@ -9,7 +9,7 @@ public class PyramidNum {
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j + " ");
 
             }
             System.out.println();
